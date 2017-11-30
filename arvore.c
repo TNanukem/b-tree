443c04a2cdeefde6e67fd824db182da54d8b9028
lucas-akira <lucas.morishita@usp.br>
2017-11-30 01:50:40 -0200

    Add comments in arvore.c, explaining the insertion process

--- arvore.c
+++ arvore.c
@@ -60,16 +60,18 @@ Pagina* verificaSplit(Pagina* P, int id, int byteOffset, int *chaveMedia, int *b
     int byte;
     Pagina* P2;
 
+		/* Retorna a posicao de id, se encontrado na pagina, ou a posicao que id
+		   deveria ficar caso nao encontrado */
     pos = procurarChave(P->numChaves, P->chaves, id);
 
     if(pos < P->numChaves && P->chaves[pos] == id) {
-        /* nothing to do */
+        /* Se achou a chave na arvore B, nao precisa inserir */
         return NULL;
     }
 
+		// Se a pagina for folha, deve-se tentar inserir id nela
     if(P->folha) {
-
-        /* everybody above pos moves up one space */
+				/* Todas as chaves maiores que id devem ser deslocados em 1 espaco para a direita*/
         memmove(&P->chaves[pos+1], &P->chaves[pos], sizeof(*(P->chaves)) * (P->numChaves - pos));
         memmove(&P->byteOffset[pos+1], &P->byteOffset[pos], sizeof(*(P->byteOffset)) * (P->numChaves - pos));
         P->chaves[pos] = id;
@@ -78,20 +80,28 @@ Pagina* verificaSplit(Pagina* P, int id, int byteOffset, int *chaveMedia, int *b
 
     } else {
 
-        /* insert in child */
+				/* Se nao for folha, chamar verificaSplit na pagina filha recursivamente
+					 ate chegar em uma pagina folha. Note que mid e byte nesse caso guardam
+					 os valores do id "promovido" (que vai para a pagina pai) e seu byteoffset,
+					 respectivamente */
         P2 = verificaSplit(P->filhos[pos], id, byteOffset, &mid, &byte);
 
-        /* maybe insert a new key in b */
+        /* Se o split foi feito: */
         if(P2) {
 
             /* every key above pos moves up one space */
+						/* Desloca as chaves para dar lugar a nova chave (id)*/
             memmove(&P->chaves[pos+1], &P->chaves[pos], sizeof(*(P->chaves)) * (P->numChaves - pos));
             /* new kid goes in pos + 1*/
+						/* Inclusive o vetor que guarda os filhos tambem deve ser deslocado em um espaco
+						   para a direita, para dar lugar ao filho de id */
             memmove(&P->filhos[pos+2], &P->filhos[pos+1], sizeof(*(P->filhos)) * (P->numChaves - pos));
 
             /* every key above pos moves up one space */
+						/* Mesmo deslocamento das chaves para os byteOffset*/
             memmove(&P->byteOffset[pos+1], &P->byteOffset[pos], sizeof(*(P->byteOffset)) * (P->numChaves - pos));
 
+						// Atualiza os valores 
             P->chaves[pos] = mid;
             P->byteOffset[pos] = byte;
             P->filhos[pos+1] = P2;
@@ -101,28 +111,39 @@ Pagina* verificaSplit(Pagina* P, int id, int byteOffset, int *chaveMedia, int *b
 
     /* we waste a tiny bit of space by splitting now
      * instead of on next insert */
+		/* Verifica se ocorreu overflow na pagina apos a insercao de id */
     if(P->numChaves >= ORDEM) {
         mid = P->numChaves/2;
 
         *chaveMedia = P->chaves[mid];
         *byteMedio = P->byteOffset[mid];
 
+				// Aloca nova pagina para receber (a segunda) metade das chaves
+				// P2 esta na mesma profundidade que o P chamado
         P2 = malloc(sizeof(Pagina));
 
+				// A nova pagina vai receber a metade dos dados menos 1 chave (a que vai ser promovida)
         P2->numChaves = P->numChaves - mid - 1;
+				// Se P for folha, entao P2 tambem sera (mesma profundidade)
         P2->folha = P->folha;
 
+				// Copia a segunda metade de P para P2; P fica com a primeira metade das chaves
         memmove(P2->chaves, &P->chaves[mid+1], sizeof(*(P->chaves)) * P2->numChaves);
+				// Analogo com os byteOffset de cada chave
         memmove(P2->byteOffset, &P->byteOffset[mid+1], sizeof(*(P->byteOffset)) * P2->numChaves);
+
+				/* Os filhos das chaves as quais foram para P2 tambem devem ser colocados
+				   em P2 */
         if(!P->folha) {
             memmove(P2->filhos, &P->filhos[mid+1], sizeof(*(P->filhos)) * (P2->numChaves + 1));
         }
-
+				// P agora fica com a primeira metade das chaves, e por consequencia o numero de
+				// chaves cai pela metade
         P->numChaves = mid;
 
         return P2;
     }
-    else {
+    else { // Se nao teve overflow, retornar nulo (nao sera necessario criar nova pagina)
         return NULL;
     }
 }
@@ -136,7 +157,8 @@ void inserirId(Pagina* P, int id, int byteOffset) {
 		// Verifica se o split deve ser feito, e o faz em caso afirmativo
     P2 = verificaSplit(P, id, byteOffset, &chaveMedia, &byteMedio);
 
-		// Se split foi feito, deve-se colocar a outra pagina
+		/* Se split foi feito na raiz (para isso P2 deve ser nao-nulo nessa linha),
+		   deve-se criar uma nova raiz */
     if(P2) {
         /* basic issue here is that we are at the root */
         /* so if we split, we have to make a new root */
